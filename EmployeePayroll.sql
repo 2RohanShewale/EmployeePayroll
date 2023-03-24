39d3c6856a00dd3b9557d2c9b7467f223bf148ea
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-24 08:43:37 +0530

    [Rohan]Ability to insert into table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -9,3 +9,15 @@ CREATE TABLE employee_payroll
    salary MONEY DEFAULT 1000,
    startDate DATETIME DEFAULT GetDate()
 );
+
+SELECT * From employee_payroll;
+
+--Insert Table
+INSERT INTO employee_payroll(name, salary, startDate) VALUES
+('Rohan', 20000, '2023-04-10'),
+('Mandar', 20000, '2023-03-11'),
+('Shubham', 30000, '2020-10-15'),
+('Priya', 20000, '2023-01-6'),
+('Aditya', 20000, '2021-02-14'),
+('John', 30000, '2020-8-15'),
+('Esha', 20000, '2022-01-6');
