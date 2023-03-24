cbd21d92e84e32c6e9a0b2c47b7f8082b0d45745
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-24 08:31:34 +0530

    [Rohan]Ability to create table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,3 +1,11 @@
 --Creating Database PayrollService
 CREATE DATABASE Payroll_Service_DB;
 
+--Create table
+CREATE TABLE employee_payroll 
+(
+	id INT IDENTITY PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   salary MONEY DEFAULT 1000,
+   startDate DATETIME DEFAULT GetDate()
+);
