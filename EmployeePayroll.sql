0dfb30304933827cc5b0d8aa9fbcced90a023d8b
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-25 17:08:26 +0530

    [Rohan]Ability to add tables and set values

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -29,3 +29,14 @@ SELECT * FROM employee_payroll;
 SELECT salary FROM employee_payroll WHERE name = 'Rohan';
 --joined
 SELECT * FROM employee_payroll WHERE startDate BETWEEN CAST('2021-01-01' AS DATE) AND GETDATE();
+
+--UC6 add column gender and set gender
+ALTER TABLE employee_payroll ADD gender varchar(1);
+
+update employee_payroll set gender = 'M' where name = 'Rohan';
+update employee_payroll set gender = 'M' where name = 'Mandar';
+update employee_payroll set gender = 'M' where name = 'Shubham';
+update employee_payroll set gender = 'F' where name = 'Aditya';
+update employee_payroll set gender = 'F' where name = 'John';
+update employee_payroll set gender = 'F' where name = 'Priya';
+update employee_payroll set gender = 'F' where name = 'Esha';
